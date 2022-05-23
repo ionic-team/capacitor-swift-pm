bc5875bb6331ab3478ed687097f8d2421a2d5d03
Steven Sherry <steven.sherry@ionic.io>
2022-05-23 15:54:56 -0500

    Add Capacitor and Cordova 3.5.1

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Capacitor",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.4.3/Capacitor.xcframework.zip",
-            checksum: "c54a0c7ddb7a887fdb741c933596bc6eb18741f1b17a404f2acc18e78a1094a1"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.5.1/Capacitor.xcframework.zip",
+            checksum: "cc5d74b531ca40ba6ed4deca3bd5bb20aa3727684d8965ae94b9f075a19f4a37"
         ),
         .binaryTarget(
             name: "Cordova",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.4.3/CapacitorCordova.xcframework.zip",
-            checksum: "903aae3aa3b36a6c294ab87732aa43ce76fe47dfa3c08b5c958749fa5c706032"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.5.1/CapacitorCordova.xcframework.zip",
+            checksum: "85f0a7a523f073cc55af7e02c8e4b9322e2681fe012813a32015e9304e5ee1c1"
         )
     ]
 )

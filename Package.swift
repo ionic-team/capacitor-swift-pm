327984e52ed3ccf17003f5d8cf6afc8b5546422c
Steven Sherry <steven.sherry@ionic.io>
2022-06-21 11:59:23 -0500

    fix: Add workaround for embedded Cordova in Capacitor.framework feat: Update to Capacitor 3.6.0

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Capacitor",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.5.1/Capacitor.xcframework.zip",
-            checksum: "cc5d74b531ca40ba6ed4deca3bd5bb20aa3727684d8965ae94b9f075a19f4a37"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.6.0/Capacitor.xcframework.zip",
+            checksum: "9ce71c655abfbed8996fab268138276cff6f5e6d52a617813d860a706c041788"
         ),
         .binaryTarget(
             name: "Cordova",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.5.1/CapacitorCordova.xcframework.zip",
-            checksum: "85f0a7a523f073cc55af7e02c8e4b9322e2681fe012813a32015e9304e5ee1c1"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.6.0/CapacitorCordova.xcframework.zip",
+            checksum: "deb7c4bdae053163061305daa6d78abb91ebdfef456136719aa97dd941de7470"
         )
     ]
 )

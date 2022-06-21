d8db401a7b6db4b7f77929e6cfb66182206b408f
Steven Sherry <steven.sherry@ionic.io>
2022-06-21 12:04:22 -0500

    fix rename CapacitorCordova to Cordova

--- Package.swift
+++ Package.swift
@@ -19,12 +19,12 @@ let package = Package(
         .binaryTarget(
             name: "Capacitor",
             url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.6.0/Capacitor.xcframework.zip",
-            checksum: "9ce71c655abfbed8996fab268138276cff6f5e6d52a617813d860a706c041788"
+            checksum: "a570e02f52de5e36582d431d1b85ac99ba1409b97a030ef31acacd55a7a9b40d"
         ),
         .binaryTarget(
             name: "Cordova",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.6.0/CapacitorCordova.xcframework.zip",
-            checksum: "deb7c4bdae053163061305daa6d78abb91ebdfef456136719aa97dd941de7470"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.6.0/Cordova.xcframework.zip",
+            checksum: "54ba63b6413736d16f7e9daa3328e251ed5e6f595e335b1cab3dc7924284535d"
         )
     ]
 )

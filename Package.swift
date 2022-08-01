c108ae317c35e88dc6d639b8f798f76f55b8f2c2
Steven Sherry <steven.sherry@ionic.io>
2022-08-01 15:59:56 -0500

    Release 3.7.0 (#4)
    * chore: Update pre-release workflow and build-cap script.
    
    Co-authored-by: Steven0351 <Steven0351@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -12,19 +12,19 @@ let package = Package(
         .library(
             name: "Cordova",
             targets: ["Cordova"]
-        ),
+        )
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
             name: "Capacitor",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.6.0/Capacitor.xcframework.zip",
-            checksum: "311a29907502e995fad871428f4889f8c156c43566dc6d616f02d94cdd944197"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.7.0/Capacitor.xcframework.zip",
+            checksum: "233a76f8b5c2a3888d78b055b9d5d2f5a781e213442df55b0bc6db6de6419b34"
         ),
         .binaryTarget(
             name: "Cordova",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.6.0/Cordova.xcframework.zip",
-            checksum: "9f562768d83888219ed90840f0d9c0f4e47105d3c45382257339282e8a4ebf9c"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/3.7.0/Cordova.xcframework.zip",
+            checksum: "5489089d27d40ec0e9dc9e2522aafabcba44475200c98459508c6ccfe2474841"
         )
     ]
 )

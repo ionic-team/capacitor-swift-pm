07ef99c8682ba55c9c888ed36b93f5735c904c4c
Steven Sherry <steven.sherry@ionic.io>
2023-09-14 09:38:15 -0500

    Release 5.2.1

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Capacitor",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/5.1.1/Capacitor.xcframework.zip",
-            checksum: "e96f3618fabe763125247d7c16d1f8223853175f8a4e17e0976651cbf6526768"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/5.2.1/Capacitor.xcframework.zip",
+            checksum: "8fd37f920908f17a6983baa438ed55f134e26b10e49cb927d807601943fbe4f0"
         ),
         .binaryTarget(
             name: "Cordova",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/5.1.1/Cordova.xcframework.zip",
-            checksum: "6a50c699d161aabbd4dd3e7cc1ff28aa30e8bc62152effd187e15a0efd66f1e9"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/5.2.1/Cordova.xcframework.zip",
+            checksum: "e3bde324d59affda5b2ee5c4770657dd512ec07e011cae4e9bd5ea0190c6e730"
         )
     ]
 )

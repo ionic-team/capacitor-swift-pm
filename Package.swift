8f7e7c53fe74d331dac609ab6d0c5ec76d10fb0d
vmfo22 <vmfo22@users.noreply.github.com>
2024-06-11 16:57:09 +0000

    Commit from GitHub Actions (Dispatch Release)

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Capacitor",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/6.0.0/Capacitor.xcframework.zip",
-            checksum: "b02aba0d30ac066cfc9091e88428cce259b81584166918a9e0bbdfef6bf83c12"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/6.1.0/Capacitor.xcframework.zip",
+            checksum: "25a77b372e0fbf8a8f0f151c8ef5c251d714b696b30c8ba65b3616068319a3fe"
         ),
         .binaryTarget(
             name: "Cordova",
-            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/6.0.0/Cordova.xcframework.zip",
-            checksum: "a403df12608583bed48a45cc5a3a0583a4b27a7f7287a8307cf430518e67ea1b"
+            url: "https://github.com/ionic-team/capacitor-swift-pm/releases/download/6.1.0/Cordova.xcframework.zip",
+            checksum: "41fe1f9f79c920076692676b87f95c0d98ac22edb797ed9405df4b8b06c96844"
         )
     ]
 )
